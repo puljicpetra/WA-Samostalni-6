7d51fc2280ab340ac17818932427f92a2a4ab165
Petra Puljić <ppuljicc@gmail.com>
2025-01-09 12:41:12 +0100

    express-validator
    POST /movies & PATCH /movies/:id

--- routes/movies.js
+++ routes/movies.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import { pretrazivanjeMoviesPoId } from '../middleware/movies.js';
+import { body, validationResult } from 'express-validator';
 
 const router = express.Router();
 
@@ -27,51 +28,81 @@ export let movies = [
   },
 ];
 
-router.get('/', async (req, res) => {
+router.get('/', (req, res) => {
   if (movies) {
     return res.status(200).json(movies);
   }
   return res.status(404).json({ message: 'Nema filma' })
 });
 
-router.get('/:id', pretrazivanjeMoviesPoId, async (req, res) => {
+router.get('/:id', pretrazivanjeMoviesPoId, (req, res) => {
   return res.status(200).json(req.movie);
 });
 
-router.post('/', async (req, res) => {
-  const { title, year, genre, director } = req.body;
-  if (!title || !year || !genre || !director) {
-    return res.status(400).json({ message: "Nedostaju podaci" });
-  }
+router.post(
+  '/', 
 
-  const noviMovie = {
-    id: Date.now(),
-    title,
-    year,
-    genre,
-    director,
-  };
+  [
+    body('title').isString().withMessage('Title treba biti string'),
+    body('year').isInt().withMessage('Year treba biti integer'),
+    body('genre').isString().withMessage('Genre treba biti string'),
+    body('director').isString().withMessage('Director treba biti string'),
+  ],
 
-  movies.push(noviMovie);
-  res.status(201).json({ message: "Uspjesno dodano", movie: noviMovie });
+  (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    const { title, year, genre, director } = req.body;
+    if (!title || !year || !genre || !director) {
+      return res.status(400).json({ message: "Nedostaju podaci" });
+    }
+
+    const noviMovie = {
+      id: Date.now(),
+      title,
+      year,
+      genre,
+      director,
+    };
+
+    movies.push(noviMovie);
+    res.status(201).json({ message: "Uspjesno dodano", movie: noviMovie });
 });
 
-router.patch('/:id', async (req, res) => {
-  const id_route_param = parseInt(req.params.id);
-  const movie = movies.find(movie => movie.id === id_route_param);
+router.patch(
+  '/:id', 
 
-  if (!movie) {
-    return res.status(404).json({ message: 'Nije pronadjen' });
-  }
+  [
+    body('title').optional().isString().withMessage('Naslov mora biti string.'),
+    body('year').optional().isInt().withMessage('Godina mora biti integer.'),
+    body('genre').optional().isString().withMessage('Žanr mora biti string.'),
+    body('director').optional().isString().withMessage('Redatelj mora biti string.'),
+  ],
+  
+  (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
 
-  const { title, year, genre, director } = req.body;
-  if (title) movie.title = title;
-  if (year) movie.year = year;
-  if (genre) movie.genre = genre;
-  if (director) movie.director = director;
+    const id_route_param = parseInt(req.params.id);
+    const movie = movies.find(movie => movie.id === id_route_param);
 
-  console.log(movies);
-  return res.status(200).json({ message: 'Uspjesno azurirano', movie });
+    if (!movie) {
+      return res.status(404).json({ message: 'Nije pronadjen' });
+    }
+
+    const { title, year, genre, director } = req.body;
+    if (title) movie.title = title;
+    if (year) movie.year = year;
+    if (genre) movie.genre = genre;
+    if (director) movie.director = director;
+
+    console.log(movies);
+    return res.status(200).json({ message: 'Uspjesno azurirano', movie });
 });
 
 export default router;
